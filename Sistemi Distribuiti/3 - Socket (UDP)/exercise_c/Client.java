44f470ec8033a5b812f94bce37a88c07616c766d
BmmNdr <abiemmibi@gmail.com>
2026-04-09 17:58:09 +0200

    SD: Terzo LAB esercizio C

--- Sistemi Distribuiti/3 - Socket (UDP)/exercise_c/Client.java
+++ Sistemi Distribuiti/3 - Socket (UDP)/exercise_c/Client.java
@@ -6,9 +6,29 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         final var serverPort = 8080;
-
+        
         try (var socket = new DatagramSocket(); var scanner = new Scanner(System.in)) {
-            var serverAddress = InetAddress.getByName("127.0.0.1");
+            final var serverAddress = InetAddress.getByName("127.0.0.1");
+
+            while (true) {
+                System.out.print("Inserisci un numero da indovinare: ");
+                var clientGuess = Integer.toString(scanner.nextInt());
+
+                var payload = clientGuess.getBytes();
+                var datagram = new DatagramPacket(payload, payload.length, serverAddress, serverPort);
+                socket.send(datagram);
+
+                var buff = new byte[1024];
+                datagram = new DatagramPacket(buff, buff.length);
+                socket.receive(datagram);
+
+                var message = new String(datagram.getData(), 0, datagram.getLength());
+                System.out.printf("Esito: %s\n", message);
+
+                if (message.equals("Indovinato"))
+                    break;
+            }
+
 
         } catch (Exception e) {
             e.printStackTrace();
--- Sistemi Distribuiti/3 - Socket (UDP)/exercise_c/Server.java
+++ Sistemi Distribuiti/3 - Socket (UDP)/exercise_c/Server.java
@@ -8,13 +8,44 @@ public class Server {
     public static void main(String[] args) {
         final var port = 8080;
         final var random = new Random();
+        final Map<Integer, Integer> targetNumbers = new HashMap<>();
 
         System.out.println("Server in ascolto...");
         try (DatagramSocket serverSocket = new DatagramSocket(port)) {
-            var targetNumber = random.nextInt(101); // 0-100.
+            var buff = new byte[1024];
+
             while (true) {
- 
+
+                var datagram = new DatagramPacket(buff, buff.length);
+                serverSocket.receive(datagram);
+
+                var clientPort = datagram.getPort();
+                var clientIP = datagram.getAddress();
+                var message = new String(datagram.getData(), 0, datagram.getLength());
+                var clientGuess = Integer.parseInt(message);
+
+                var toGuess = targetNumbers.getOrDefault(clientPort, 0);
+                if (toGuess == 0){
+                    toGuess = random.nextInt(101);
+                    targetNumbers.put(clientPort, toGuess);
+                }
+            
+                System.out.printf("Server: %d\tClient (%d): %d\n", toGuess, clientPort, clientGuess);
+
+                if (clientGuess > toGuess)
+                    message = "Troppo Alto";
+                else if (clientGuess < toGuess)
+                    message = "Troppo Basso";
+                else {
+                    message = "Indovinato";
+                    targetNumbers.remove(clientPort);
+                }
+
+                var payload = message.getBytes();
+                datagram = new DatagramPacket(payload, payload.length, clientIP, clientPort);
+                serverSocket.send(datagram);
             }
+        
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
